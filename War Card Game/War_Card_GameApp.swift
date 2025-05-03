845feb1fcf5fdb9f51c09a095697e68cd9ea827f
sharuban <s.s.ruban98@gmail.com>
2025-05-03 19:51:36 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Sharuban Sharu on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
